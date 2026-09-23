0b8587a1e32c3751abf03372bfca06f929840d48
Kelly <kellyduranmarcelo1234@gmail.com>
2026-09-23 09:27:39 -0100

    Fix: Corrección de compilación FCM, copia en HTTP sin SSL y WebSockets de roles/candados

--- src/app/notifications/fcm.service.ts
+++ src/app/notifications/fcm.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-const initializeApp = (...args: any[]) => ({});
+import { initializeApp } from 'firebase/app';
 import { getMessaging, getToken, isSupported } from 'firebase/messaging';
 import { firstValueFrom } from 'rxjs';
 import { BASE_PATH } from '../api/variables';
